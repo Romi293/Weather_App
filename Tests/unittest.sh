e5024b6fb8bef6477d957718a99dd50d4581d613
Romi.Geora <Romi29396@gmail.com>
2024-06-24 16:43:36 +0300

    Adding weather app project

---
+++ Tests/unittest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ "$1" ]];then
+  IP="$1"
+else
+  IP=$(hostname -i)
+fi
+echo "$IP"
+curl http://"$IP":8000 # -o /dev/null -s
+RESULT=$?
+if [[ RESULT -eq 0 ]]; then
+    echo "Command succeeded"
+    echo "Exit Code" $RESULT
+else
+    echo "Command failed"
+    echo "Exit Code" $RESULT
+    exit 1
+fi
+exit 0
